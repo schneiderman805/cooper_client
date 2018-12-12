6101c7a3c9a708bbbd0602670cd4d40c07a8cc90
William Schneiderman <william.m.schneiderman@gmail.com>
2018-12-12 13:22:56 +0100

    adds test for fill in form to app.e2e-spec.ts, adds fill in form function to app.po.ts and creates form in home.html

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -17,5 +17,12 @@ describe('App', () => {
         expect(title).toEqual('Page One');
       });
     });
+
+    it('fill in form', () => {
+        page.fillInForm(1000, 'Female', 20);
+        expect(page.results_card_header()).toContain('Cooper Test Result');
+        expect(page.results_card_content()).toContain('Gender: female, Age: 20  Result: Poor');
+    });
+    
   })
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -13,4 +13,34 @@ export class Page {
  getPageOneTitleText() {
     return element(by.tagName('page-one')).element(by.tagName('ion-title')).getText();
   }
+
+  fillInForm(distance, gender, age) {
+
+    element(by.css('.text-input')).clear().then(() => {  
+        element(by.css('.text-input')).sendKeys(distance); 
+    }) 
+
+    element(by.css('.select')).click().then(() => {  
+        browser.sleep(500); 
+
+        element(by.cssContainingText('.button-inner', gender)).click();
+        element(by.cssContainingText('.button-inner', 'OK')).click(); 
+
+    })
+
+    browser.sleep(500); 
+
+    element(by.cssContainingText('.button', 'Calculate')).click();  
+
+    }
+
+    results_card_header() {
+        return element(by.css('ion-card-header')).getText();
+    } 
+
+    results_card_content() {
+        return element(by.css('ion-card-content')).getAttribute('textContent');
+    } 
+  
 }
+
